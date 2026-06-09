3fd66f5b392f225517ecae8792c801af5c47f122
tk0bforever-cyber <tk0bforever@gmail.com>
2026-06-09 17:27:37 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-function calcular() {
-    let cantidad = document.getElementById("maruchanes").value;
+const modoBtn = document.getElementById("modoBtn");
 
-    if(cantidad <= 0 || cantidad === ""){
-        document.getElementById("resultado").innerHTML =
-        "Ingresa una cantidad válida.";
-        return;
-    }
+modoBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
 
-    let ahorro = cantidad * 1200;
+function mostrarReceta(){
 
-    document.getElementById("resultado").innerHTML =
-    `Al sustituir ${cantidad} Maruchanes tradicionales por una versión saludable podrías evitar aproximadamente ${ahorro} mg de sodio por semana.`;
+document.getElementById("recetaBox").innerHTML = `
+
+<h3>🍜 Ingredientes</h3>
+
+<p>
+✔ Fideos integrales<br>
+✔ Zanahoria<br>
+✔ Brócoli<br>
+✔ Espinaca<br>
+✔ Pollo o tofu<br>
+✔ Caldo bajo en sodio
+</p>
+
+<h3>👨‍🍳 Preparación</h3>
+
+<p>
+1. Cocina los fideos.<br>
+2. Agrega verduras frescas.<br>
+3. Añade proteína.<br>
+4. Incorpora el caldo.<br>
+5. Sirve caliente.
+</p>
+
+`;
 }
+
+function animar(id, final, velocidad){
+
+let valor = 0;
+
+let intervalo = setInterval(() => {
+
+valor++;
+
+document.getElementById(id).textContent = valor;
+
+if(valor >= final){
+clearInterval(intervalo);
+}
+
+}, velocidad);
+
+}
+
+animar("sodio",1200,2);
+animar("proteina",35,50);
+animar("fibra",60,25);
